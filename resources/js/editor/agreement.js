6668d675f45d7dd9393f1cfc28f559510bc273cb
MOSYZ0820 <lozhengyu3@gmail.com>
2026-08-25 19:31:20 +0800

    add bold version component for editor

--- resources/js/editor/agreement.js
+++ resources/js/editor/agreement.js
@@ -75,6 +75,42 @@ const agreementBlocks = [
     `
     },
     {
+        id: 'numbered-list (bold)',
+        label: 'Numbered List (Bold)',
+        category: 'Basic Elements',
+        content: `
+            <ol style="margin: 15px 0; padding-left: 20px; line-height: 1.6; color: #333; font-weight: bold;">
+                <li style="margin-bottom: 5px;">Double-click to edit numbered item 1</li>
+                <li style="margin-bottom: 5px;">Double-click to edit numbered item 2</li>
+                <li style="margin-bottom: 5px;">Double-click to edit numbered item 3</li>
+            </ol>
+        `
+    },
+    {
+        id: 'alphabetical-list (bold)',
+        label: 'Alphabetical List (Bold)',
+        category: 'Basic Elements',
+        content: `
+            <ol style="list-style-type: upper-alpha; margin: 15px 0; padding-left: 20px; line-height: 1.6; color: #333; font-weight: bold;">
+                <li style="margin-bottom: 5px;">Double-click to edit item A</li>
+                <li style="margin-bottom: 5px;">Double-click to edit item B</li>
+                <li style="margin-bottom: 5px;">Double-click to edit item C</li>
+            </ol>
+        `
+    },
+    {
+        id: 'lower-alphabetical-list (bold)',
+        label: 'Lower Alphabetical List (Bold)',
+        category: 'Basic Elements',
+        content: `
+            <ol style="list-style-type: lower-alpha; margin: 15px 0; padding-left: 20px; line-height: 1.6; color: #333; font-weight: bold;">
+                <li style="margin-bottom: 5px;">Double-click to edit item a</li>
+                <li style="margin-bottom: 5px;">Double-click to edit item b</li>
+                <li style="margin-bottom: 5px;">Double-click to edit item c</li>
+            </ol>
+        `
+    },
+    {
         id: 'basic-table',
         label: 'Simple Table',
         category: 'Basic Elements',
